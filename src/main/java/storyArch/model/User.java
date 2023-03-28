0f2ec321b0b228b886fdec203dfbf49c6673529c
apm30 <m.ajaypradeep@icloud.com>
2023-03-28 11:18:11 +0100

    Creating a simple user menu for the view and updating the data model for the User.

--- src/main/java/storyArch/model/User.java
+++ src/main/java/storyArch/model/User.java
@@ -1,4 +1,61 @@
 package main.java.storyArch.model;
 
+/**
+ *  User class - Data model for the user of the application
+ */
 public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String username;
+
+    public User(String firstName, String lastName, String email, String password, String username) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
--- src/main/java/storyArch/view/CommandLine.java
+++ src/main/java/storyArch/view/CommandLine.java
@@ -1,4 +1,24 @@
 package main.java.storyArch.view;
 
+// Command Line View for Story Arch application
 public class CommandLine {
+
+  public void main (String[] args){
+    System.out.println("Hello User!");
+    start();
+  }
+
+  public void start() {
+    System.out.println("Welcome to Story Arch!");
+    System.out.println("Please select an option:");
+    System.out.println("1. Login");
+    System.out.println("2. Register");
+    System.out.println("3. Exit");
+  }
+
+
+
+
+
+
 }
