23a31405503f6978140592b28ccb77a9d580bdb2
apm30 <m.ajaypradeep@icloud.com>
2023-03-28 14:26:43 +0100

    Implemented Invoice (extends User) and generated getter and setter.

--- src/main/java/storyArch/model/Invoice.java
+++ src/main/java/storyArch/model/Invoice.java
@@ -1,4 +1,82 @@
 package main.java.storyArch.model;
 
-public class Invoice {
+import java.util.Date;
+
+/**
+ * Invoice class stores the user's invoice information
+ * It extends the User class
+ */
+public class Invoice extends User{
+    private int invoiceID;
+    private String currency;
+
+    private float total;
+
+    private Date  sentDate;
+
+    private Date dueDate;
+
+    private Date paidDate;
+
+    public Invoice(String fullName, String email, String password, String TOTP_TOKEN, String AUTH_TOKEN, String marketBiography, byte[] profilePicture) {
+        super(fullName, email, password, TOTP_TOKEN, AUTH_TOKEN, marketBiography, profilePicture);
+    }
+
+    public Invoice(String fullName, String email, String password, String TOTP_TOKEN, String AUTH_TOKEN, String marketBiography, byte[] profilePicture, int invoiceID, String currency, float total, Date sentDate, Date dueDate, Date paidDate) {
+        super(fullName, email, password, TOTP_TOKEN, AUTH_TOKEN, marketBiography, profilePicture);
+        this.invoiceID = invoiceID;
+        this.currency = currency;
+        this.total = total;
+        this.sentDate = sentDate;
+        this.dueDate = dueDate;
+        this.paidDate = paidDate;
+    }
+
+    public int getInvoiceID() {
+        return invoiceID;
+    }
+
+    public void setInvoiceID(int invoiceID) {
+        this.invoiceID = invoiceID;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    public Date getSentDate() {
+        return sentDate;
+    }
+
+    public void setSentDate(Date sentDate) {
+        this.sentDate = sentDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getPaidDate() {
+        return paidDate;
+    }
+
+    public void setPaidDate(Date paidDate) {
+        this.paidDate = paidDate;
+    }
 }
--- src/main/java/storyArch/model/User.java
+++ src/main/java/storyArch/model/User.java
@@ -15,4 +15,70 @@ public class User {
     private String marketBiography;
 
     private byte[] profilePicture;
+
+    public User(String fullName, String email, String password, String TOTP_TOKEN, String AUTH_TOKEN, String marketBiography, byte[] profilePicture) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.TOTP_TOKEN = TOTP_TOKEN;
+        this.AUTH_TOKEN = AUTH_TOKEN;
+        this.marketBiography = marketBiography;
+        this.profilePicture = profilePicture;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTOTP_TOKEN() {
+        return TOTP_TOKEN;
+    }
+
+    public void setTOTP_TOKEN(String TOTP_TOKEN) {
+        this.TOTP_TOKEN = TOTP_TOKEN;
+    }
+
+    public String getAUTH_TOKEN() {
+        return AUTH_TOKEN;
+    }
+
+    public void setAUTH_TOKEN(String AUTH_TOKEN) {
+        this.AUTH_TOKEN = AUTH_TOKEN;
+    }
+
+    public String getMarketBiography() {
+        return marketBiography;
+    }
+
+    public void setMarketBiography(String marketBiography) {
+        this.marketBiography = marketBiography;
+    }
+
+    public byte[] getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(byte[] profilePicture) {
+        this.profilePicture = profilePicture;
+    }
 }
