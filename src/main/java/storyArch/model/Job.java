af6aff8325c1f567b1836514541ecd79b3c6fdb7
apm30 <m.ajaypradeep@icloud.com>
2023-03-28 14:44:22 +0100

    Implemented Chapter MODEL (extends Job which extends User).

--- src/main/java/storyArch/model/Job.java
+++ src/main/java/storyArch/model/Job.java
@@ -9,13 +9,13 @@ import java.util.Date;
  */
 
 public class Job extends Invoice {
-    private final int ID;
+    private int ID;
 
-    private final String description;
+    private String description;
 
-    private final Date publishedDate;
+    private Date publishedDate;
 
-    private final Date completedDate;
+    private Date completedDate;
 
     public Job(String fullName, String email, String password, String TOTP_TOKEN, String AUTH_TOKEN, String marketBiography, byte[] profilePicture, int invoiceID, String currency, float total, Date sentDate, Date dueDate, Date paidDate, int ID, String description, Date publishedDate, Date completedDate) {
         super(fullName, email, password, TOTP_TOKEN, AUTH_TOKEN, marketBiography, profilePicture, invoiceID, currency, total, sentDate, dueDate, paidDate);
@@ -25,19 +25,36 @@ public class Job extends Invoice {
         this.completedDate = completedDate;
     }
 
+
     public int getID() {
         return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public Date getPublishedDate() {
         return publishedDate;
     }
 
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
     public Date getCompletedDate() {
         return completedDate;
+    }
+
+    public void setCompletedDate(Date completedDate) {
+        this.completedDate = completedDate;
     }
 }
